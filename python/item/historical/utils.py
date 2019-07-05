fcf4274956ab2f2c7bb6f4225b62d817fa3a8358
TheYuanLiao <30661239+TheYuanLiao@users.noreply.github.com>
2019-07-05 16:29:19 +0200

    Create utils.py
    Raw code for merging raw datasets into one file with standard columns and units.

---
+++ python/item/historical/utils.py
@@ -0,0 +1,82 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+import yaml
+# Define the function used for converting a unit from raw to standard in iTEM3 template
+def unit_conversion(unitA, unitB):
+    # Convert unitA to unitB
+    if (unitA, unitB) == ("Gigawatt-hour", 'PJ / yr'):
+        func = lambda x: x * 0.0036
+    if (unitA, unitB) == ("Thousand TOE (tonnes of oil equivalent)",'PJ / yr'):
+        func = lambda x: x * 0.0419
+    if unitA == unitB:
+        func = lambda x: x
+    if ((unitA, unitB) == ('10⁶ tonne-km / yr', "10⁹ tonne-km / yr")) | ((unitA, unitB) == ('Million TKM (tonne-kilometre)',"10⁹ tonne-km / yr")) \
+        | ((unitA, unitB) == ("10⁶ passenger-km / yr", "10⁹ passenger-km / yr")) | ((unitA, unitB) == ("Millions of passenger-kilometres","10⁹ passenger-km / yr")):
+        func = lambda x: x / 1000
+    if ((unitA, unitB) == ("10³ tonne / yr", "10⁹ tonne / yr")) | ((unitA, unitB) == ("Number", "10⁶ vehicle / yr"))  \
+        | ((unitA, unitB) == ('Thousand tonnes', "10⁹ tonne / yr")) | ((unitA, unitB) == ("Number", "10⁶ vehicle")) \
+        | ((unitA, unitB) == ("absolute value", "10⁶ vehicle")) | ((unitA, unitB) == ("per 1000 inhabitants","10⁶ vehicle / k inhabitants")):
+        func = lambda x: x / 10**6                                                                    
+    return func
+# For adding Unit column with defined one by the raw data set
+def addUnit_conversion(row):
+    if top_dict["Added_columns_var_mapping"]["Unit"]["DEPEND"] == "None":
+        value = top_dict["Added_columns_var_mapping"]["Unit"]["MAPPING"]
+    else:
+        depended_field = top_dict["Added_columns_var_mapping"]["Unit"]["DEPEND"]
+        v = row[depended_field]
+        value = top_dict["Added_columns_var_mapping"]["Unit"]["MAPPING"][v]
+    return value
+
+# For converting Unit from raw to standard
+def value_conversion(row):
+    func = unit_conversion(row["Unit"], top_dict["Other_columns_var_mapping"]["Unit"]["MAPPING"][row["Unit"]])
+    return func(row["Value"])
+
+# Function of conversion phase 1
+def conversion_layer1(df, top_dict):
+    # Input:
+    #  df - raw data set loaded from IK2 Open Data
+    #  top_dict - raw-data-set-dependent dictionary with mapping rules loaded from mapping_conv_phase1.yaml
+    
+    # Rename existing columns that to match the template
+    df = df.loc[:, [x for x in top_dict['Raw_columns_mapping'] if x != "ADD"]]
+    col_dict = top_dict['Raw_columns_mapping']
+    map(col_dict.pop, ['ADD'])
+    df.rename(columns = col_dict, inplace = True)
+    
+    # Add columns that are not included and mapping with pre-defined rules
+    for x in top_dict['Raw_columns_mapping']["ADD"]:
+        if x != "Unit":
+            df[x] = [top_dict["Added_columns_var_mapping"][x]] * len(df)
+        else:
+            df[x] = df.apply(lambda row: addUnit_conversion(row), axis=1)
+
+    # Unit conversion if necessary
+    if "Unit" in top_dict["Other_columns_var_mapping"]:
+        df["Value"] = df.apply(lambda row: value_conversion(row), axis=1)
+    # Name conversion for some columns to keep as consistent as possible while keeping the raw data set's taxonomy
+    if "Other_columns_var_mapping" in  top_dict:
+        for field in top_dict["Other_columns_var_mapping"]:
+            if field != "Unit":
+                df[field] = df[field].apply(lambda x: top_dict["Other_columns_var_mapping"][field][x])
+            else:
+                df[field] = df[field].apply(lambda x: top_dict["Other_columns_var_mapping"][field]["MAPPING"][x])
+    return df
+
+datapath = "IK2_Open_Data/"
+dataList = os.listdir(datapath)
+dataList = [x for x in dataList if x[-4:] == ".csv"]
+top_most_dict_yaml = yaml.safe_load(open('mapping_conv_phase1.yaml'))
+list_of_df = []
+for file in top_most_dict_yaml:
+    df = pd.read_csv(datapath + file, sep = ";")
+    top_dict = top_most_dict_yaml[file] # Mapping rules for dataset
+    df = conversion_layer1(df, top_dict)
+    df["Source"] = file
+    list_of_df.append(df)
+df_output = pd.concat(list_of_df, sort=False, ignore_index=True)
+df_output = df_output[["Model", "Scenario", "Region", "Variable", "Unit", "Mode", "Technology", "Fuel", "Year", "Value", "Source"]]
+df_output.to_csv("IK2_Open_Data_conv_phase1.csv", index=False)
