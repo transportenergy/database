c486eaaff76506a27ece21fef71a398474de5219
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-07-07 19:07:54 +0200

    ci: install devtools before source

--- ci/install.sh
+++ ci/install.sh
@@ -2,3 +2,7 @@ $CACHE/$CONDAFNAME -b -u -p $HOME/miniconda
 conda info --all
 
 pip install -r ci/requirements.txt
+
+# NB must do this here because Travis does not handle 'r_binary_packages' until
+#    *after* the install: script in .travis.yml
+sudo apt-get install -y r-cran-devtools
