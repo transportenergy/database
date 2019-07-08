d46423c0eafa65768e01d57bb6a56b893ead2d71
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2019-07-08 11:56:03 +0200

    Update pytest options hook

--- python/item/tests/conftest.py
+++ python/item/tests/conftest.py
@@ -4,6 +4,15 @@ import shutil
 import pytest
 
 
+def pytest_addoption(parser):
+    parser.addoption('--run-slow', action='store_true', default=False,
+                     help='run slow tests')
+    parser.addoption('--local-data',
+                     help='path to local data for testing')
+    parser.addoption('--stats-server', default=None,
+                     help='address of statistics server')
+
+
 # From xarray
 @pytest.fixture(scope='session')
 def item_tmp_dir(tmp_path_factory):
