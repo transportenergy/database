ecbf08791dbb748aa8f625d907a613f56b58e18d
Humberto Linero <humberto_1992@msn.com>
2019-11-21 14:48:23 +0100

    making corrections based on stickler

--- item/historical/scripts/util/DataframeManager.py
+++ item/historical/scripts/util/DataframeManager.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 class DataframeManager:
 
     @classmethod
@@ -10,11 +8,16 @@ class DataframeManager:
         print(">> Number of Rows: {}".format(rows))
 
     @classmethod
-    def simple_column_insert(cls, dataframe, column_name, cell_value, position = 0):
+    def simple_column_insert(
+                            cls, dataframe,
+                            column_name, cell_value, position=0):
         column_content = [cell_value] * len(dataframe)
         dataframe.insert(position, column_name, column_content, True)
 
     @classmethod
     def reorder_columns(cls, dataframe):
-        column_order = ['Source', 'Region', 'Variable','Unit','Service','Mode','Vehicle Type','Technology','Fuel','Value','Year']
+        column_order = ['Source', 'Region', 'Variable',
+                        'Unit', 'Service', 'Mode',
+                        'Vehicle Type', 'Technology',
+                        'Fuel', 'Value', 'Year']
         return dataframe.reindex(columns=column_order)
--- item/historical/scripts/util/DatasetManager.py
+++ item/historical/scripts/util/DatasetManager.py
@@ -1,16 +1,17 @@
 import os
 import pandas as pd
 
+
 class DatasetManager:
 
     def __init__(self, dataset_id):
         self.dataset_id = dataset_id
 
-    def get_dataframe_from_csv_file(self, path_to_file, delimeter = ","):
+    def get_dataframe_from_csv_file(self, path_to_file, delimeter=","):
         if os.path.exists(path_to_file):
             return pd.read_csv(path_to_file, delimeter)
         else:
-            return pd.DataFrame({'Empty' : []})
+            return pd.DataFrame({'Empty': []})
 
     def create_user_friendly_file(self, dataframe, path):
         pass
