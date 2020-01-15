f596ef038a5822958a45285f9931066554482d43
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-01-16 00:25:12 +0100

    Add test_run_notebook to execute item.historical notebooks

--- item/tests/test_historical.py
+++ item/tests/test_historical.py
@@ -1,5 +1,11 @@
+from pathlib import Path
+
+import pytest
+
+import item
 from item.common import paths
 from item.historical import input_file
+from item.historical.util import run_notebook
 
 
 def test_import():
@@ -27,3 +33,22 @@ def test_input_file(item_tmp_dir):
 
     # input_file retrieves the last-sorted file:
     assert input_file(1) == paths['historical input'] / 'T001_foo.csv'
+
+
+# Path to IPython notebooks
+nb_path = Path(item.__file__).parent / 'historical' / 'scripts'
+
+
+@pytest.mark.parametrize('dataset_id', [
+    'T000',
+    'T001',
+    'T002',
+    'T003',
+    'T004',
+    'T005',
+    'T006',
+])
+def test_run_notebook(dataset_id, tmp_path):
+    # Notebook runs top-to-bottom without cell errors
+    nb, errors = run_notebook(nb_path / f'{dataset_id}.ipynb', tmp_path)
+    assert errors == []
