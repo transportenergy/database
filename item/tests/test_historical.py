eeaee5492e48c8e86db18f3cb0a6f2a0f3f0eca7
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-01-15 23:58:30 +0100

    Add test of importing historical.scripts.util.managers classes

--- item/tests/test_historical.py
+++ item/tests/test_historical.py
@@ -2,6 +2,20 @@ from item.common import paths
 from item.historical import input_file
 
 
+def test_import():
+    # All historical database classes can be imported
+    from item.historical.scripts.util.managers.CountryCodeManager \
+        import CountryCodeManager
+    from item.historical.scripts.util.managers.DataframeManager \
+        import DataframeManager
+    from item.historical.scripts.util.managers.UnitConverterManager \
+        import UnitConverterManager
+
+    CountryCodeManager()
+    DataframeManager()
+    UnitConverterManager()
+
+
 def test_input_file(item_tmp_dir):
     # Create some temporary files in any order
     files = [
