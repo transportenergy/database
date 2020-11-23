d84977682412f376d206dd3b3e976718cb69df1e
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2020-11-23 20:56:28 +0100

    Adjust test_historical.test_coverage for T000

--- item/tests/test_historical.py
+++ item/tests/test_historical.py
@@ -92,7 +92,7 @@ def test_run_notebook(dataset_id, tmp_path):
     assert errors == []
 
 
-@pytest.mark.parametrize("dataset_id, N_areas", [(0, 57), (1, 37), (2, 53), (3, 57)])
+@pytest.mark.parametrize("dataset_id, N_areas", [(0, 58), (1, 37), (2, 53), (3, 57)])
 def test_coverage(dataset_id, N_areas):
     """Test the historical.diagnostics.coverage method."""
     df = pd.read_csv(fetch_source(dataset_id, use_cache=True))
