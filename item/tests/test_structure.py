258edb3ef5ccaab5608edd0879db00b85d4afaf7
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-02-22 13:55:38 +0100

    Add test that the data structure description can be written and read

--- item/tests/test_structure.py
+++ item/tests/test_structure.py
@@ -1,3 +1,6 @@
+import sdmx
+
+from item.sdmx import generate
 from item.structure import column_name, make_template
 
 
@@ -19,3 +22,16 @@ def test_make_template(tmp_path):
 
     assert 2493 + 1 == sum(1 for _ in open(tmp_path / "template.csv"))
     assert 2493 + 2 == sum(1 for _ in open(tmp_path / "index.csv"))
+
+
+def test_sdmx_roundtrip(tmp_path):
+    path = tmp_path / "structure.xml"
+
+    # Structure can be written
+    with open(path, "wb") as f:
+        f.write(sdmx.to_xml(generate(), pretty_print=True))
+
+    # Structure can be read
+    sm = sdmx.read_sdmx(path)
+
+    assert 3 == len(sm.constraint["PRICE_FUEL"].data_content_keys)
