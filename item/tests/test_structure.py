989726cc852a333c8c149661b5edbf915a2f1719
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-04-12 13:04:30 +0200

    Update test_make_template()

--- item/tests/test_structure.py
+++ item/tests/test_structure.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 import sdmx
 
 from item.sdmx import generate
@@ -17,11 +19,14 @@ def test_make_template(tmp_path):
     make_template(output_path=tmp_path)
 
     # Produces 4 files
-    for name in ["index.csv", "index.xlsx", "template.csv", "template.xlsx"]:
-        assert (tmp_path / name).exists()
+    for base, suffix in product(["full", "condensed", "index"], [".csv", ".xlsx"]):
+        assert (tmp_path / base).with_suffix(suffix).exists()
 
-    assert 2493 + 1 == sum(1 for _ in open(tmp_path / "template.csv"))
-    assert 2493 + 2 == sum(1 for _ in open(tmp_path / "index.csv"))
+    # Files have the expected length
+    expected_keys = 6162
+    assert expected_keys + 1 == sum(1 for _ in open(tmp_path / "condensed.csv"))
+    assert expected_keys + 1 == sum(1 for _ in open(tmp_path / "full.csv"))
+    assert expected_keys + 2 == sum(1 for _ in open(tmp_path / "index.csv"))
 
 
 def test_sdmx_roundtrip(tmp_path):
