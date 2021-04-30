acec590fec85b6a6d05c21646384ae60ad08183b
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-04-30 20:32:37 +0200

    Complete code for T007

--- item/historical/T007.py
+++ item/historical/T007.py
@@ -1,10 +1,20 @@
+from functools import lru_cache
+
+import pandas as pd
+
+from item.structure import column_name
+
+#: Separator character for :func:`pandas.read_csv`.
+CSV_SEP = ";"
+
 #: Dimensions and attributes which do not vary across this data set.
 COMMON_DIMS = dict(
     source="Eurostat",
     service="Passenger",
     technology="All",
     fuel="All",
-    variable="Activity",
+    variable="Activity, share of distance",
+    unit="percent",
 )
 
 #: Columns to drop from the raw data.
@@ -20,13 +30,18 @@ def check(df):
 
 
 def process(df):
-    raise NotImplementedError
-    # TODO rename "Geo" → "Country"
-    # TODO rename "Date" → "Year"
-    # TODO map the following:
-    # - Vehicle --> Mode --> Vehicle Type
-    # - Trains --> Rail --> All
-    # - Passenger cars --> Road --> LDV
-    # - Motor coaches, buses and trolley buses --> Road --> Bus
-    # TODO the Jupyter notebook assigned units "% in total inland passenger-km / yr"
-    #      —it's unclear what this means. Check.
+    return pd.concat([df, df["Vehicle"].apply(map_mode_vehicle_type)], axis=1).rename(
+        columns=dict(Geo=column_name("COUNTRY"), Date=column_name("YEAR"))
+    )
+
+
+@lru_cache
+def map_mode_vehicle_type(value):
+    return pd.Series(
+        {
+            "Trains": ["Rail", "All"],
+            "Passenger cars": ["Road", "LDV"],
+            "Motor coaches, buses and trolley buses": ["Road", "LDV"],
+        }.get(value),
+        index=[column_name("MODE"), column_name("VEHICLE_TYPE")],
+    )
