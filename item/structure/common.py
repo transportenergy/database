358150a6c30c8754f10ebd83691eafb69954378b
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-04-30 20:31:39 +0200

    Check the DSD *first* in .structure.column_name()

--- item/structure/common.py
+++ item/structure/common.py
@@ -1,6 +1,7 @@
 import logging
 from functools import lru_cache
 
+from item.historical.util import ColumnName
 from item.structure.sdmx import generate
 
 log = logging.getLogger(__name__)
@@ -10,13 +11,13 @@ log = logging.getLogger(__name__)
 def column_name(id: str) -> str:
     """Return a human-readable name for a dimension in the historical DSD."""
     try:
-        value = dict(VARIABLE="Variable", YEAR="Year", ID="ID")[id]
-        log.warning(f"Deprecated dimension id: {repr(id)}")
-        return value
-    except KeyError:
         return (
             generate()
             .structure["HISTORICAL"]
             .dimensions.get(id.upper())
             .concept_identity.name.localized_default()  # type: ignore [union-attr]
         )
+    except (AttributeError, KeyError):
+        value = ColumnName[id.upper()].value
+        log.warning(f"Deprecated dimension id: {repr(id)}")
+        return value
