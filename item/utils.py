ca8026795bb00f6fdfd53ae2e1e36d8e49337416
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-05-04 00:10:59 +0200

    Make .utils.convert_units flexible w.r.t. "value", "VALUE", etc.

--- item/utils.py
+++ item/utils.py
@@ -1,9 +1,14 @@
+from typing import Optional, Sequence
+
+import pandas as pd
 from iam_units import registry
 
 
 # TODO Add an argument to control the format of the output units
-def convert_units(df, units_from, units_to, cols=("Value", "Unit")):
-    """Convert units of *df*.
+def convert_units(
+    df: pd.DataFrame, units_from, units_to, cols: Optional[Sequence[str]] = None
+) -> pd.DataFrame:
+    """Convert units of `df`.
 
     Uses a vector :class:`registry.Quantity` to convert an entire column of values
     efficiently.
@@ -15,13 +20,21 @@ def convert_units(df, units_from, units_to, cols=("Value", "Unit")):
     units_to : str or pint.Unit
         Units to convert to.
     cols : 2-tuple of str
-        Names of the columns in *df* containing the magnitude and unit,
-        respectively.
+        Names of the columns in `df` containing the magnitude and unit, respectively.
+        Default for the mangnitude is whichever column of `df` matches “value”, case-
+        insensitive; if multiple columns have different cases of this name, the first
+        is used.
 
     Returns
     -------
     pandas.DataFrame
     """
+    # Default values
+    cols = cols or [
+        next(filter(lambda name: name.lower() == "value", df.columns)),
+        "UNIT",
+    ]
+
     # Create a vector pint.Quantity; convert units
     qty = registry.Quantity(df[cols[0]].values, units_from).to(units_to)
 
