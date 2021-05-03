69bb09b7f9e8ff8670f4f892b0efc186f8df98a9
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-05-04 00:39:03 +0200

    Add DATAFLOW to T005 and simplify

--- item/historical/T005.py
+++ item/historical/T005.py
@@ -1,60 +1,56 @@
 from functools import lru_cache
 
-from item.structure import column_name
 from item.utils import convert_units
+
+#: iTEM data flow matching the data from this source.
+DATAFLOW = "EMISSIONS"
 
 #: Dimensions and attributes which do not vary across this data set.
 COMMON_DIMS = dict(
     source="JRC",
-    variable="CO₂ emission (ttw)",
-    technology="All",
-    fuel="All",
-    vehicle_type="All",
-    service="All",
+    variable="Emissions",
+    pollutant="CO2",
+    lca_scope="TTW",
+    service="_T",
+    vehicle="_T",
+    fuel="_T",
+    technology="_T",
 )
 
 #: Columns to drop from the raw data.
 COLUMNS = dict(
-    drop=["IPCC_description", "IPCC-Annex", "World Region"],
+    drop=["IPCC_description", "IPCC-Annex", "Name", "World Region"],
 )
+
+
+MAP_MODE = {
+    "1.A.3.a": "Air",  # "Civil Aviation"
+    "1.A.3.b": "Road",  # "Road Transportation"
+    "1.A.3.c": "Rail",  # "Railways"
+    "1.A.3.d": "Water",  # "Water-borne Navigation"
+    "1.A.3.e": "Other",  # "Other Transportation"
+}
 
 
 def process(df):
     """Process T005.
 
     1. Select only measures with IDs beginning "1.A.3".
-    2. Melt from wide to long format.
-    3. Drop NA values.
-    4. Map from the IPCC emissions category (e.g. "1.A.3.a") to mode (e.g. "Air");
-       see :func:`map_mode`.
-    5. Convert from Mt/a to Gt/a.
+    2. Map from the IPCC emissions category (e.g. "1.A.3.a") to mode (e.g. "Air"); see
+       :func:`map_mode`.
+    3. Melt from wide to long format.
+    4. Drop NA values.
+    5. Use “_X” (not allocated/unspecified) as the region for international shipping and
+       aviation.
+    6. Convert from Mt/a to Gt/a.
     """
-    # TODO use the following
-    # - Int. Aviation --> World
-    # - Int. Shipping --> World
-
     return (
         df[df["IPCC"].str.startswith("1.A.3")]
-        .melt(
-            id_vars=["ISO_A3", "Name", "IPCC"],
-            var_name=column_name("YEAR"),
-            value_name=column_name("VALUE"),
-        )
-        .dropna(subset=[column_name("VALUE")])
-        .rename(
-            columns={"ISO_A3": column_name("ISO_CODE"), "Name": column_name("COUNTRY")}
-        )
-        .assign(mode=lambda df: df["IPCC"].apply(map_mode))
+        .assign(MODE=lambda df_: df_["IPCC"].apply(MAP_MODE.get))
+        .drop(columns=["IPCC"])
+        .melt(id_vars=["ISO_A3", "MODE"], var_name="TIME_PERIOD", value_name="VALUE")
+        .dropna(subset=["VALUE"])
+        .rename(columns={"ISO_A3": "REF_AREA"})
+        .replace({"REF_AREA": {"SEA": "_X", "AIR": "_X"}})
         .pipe(convert_units, "megatonne / year", "gigatonne / year")
     )
-
-
-@lru_cache()
-def map_mode(value):
-    return {
-        "1.A.3.a": "Air",  # "Civil Aviation"
-        "1.A.3.b": "Road",  # "Road Transportation"
-        "1.A.3.c": "Rail",  # "Railways"
-        "1.A.3.d": "Water",  # "Water-borne Navigation"
-        "1.A.3.e": "Other",  # "Other Transportation"
-    }.get(value)
