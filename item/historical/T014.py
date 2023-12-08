0d99d0b835d7db5162b73c746a7150767751199f
Dima <122933749+Dima-Faraji@users.noreply.github.com>
2023-12-08 07:31:22 +0100

    Add files via upload

--- item/historical/T014.py
+++ item/historical/T014.py
@@ -240,7 +240,11 @@ class AtoWorkbook:
     def update_master_data(self, df_out_put: pd.DataFrame, df: pd.DataFrame, column_list_names,
                         upper_attributes, remaining_attributes, regions):
         #[mode_value, source_value, service_value, unit_value, indicator_value, sheet_name]
-        #[vehicle_type, variable_type, unit, unit_factor, rule_id]
+        #[vehicle_type, variable_type, unit, unit_factor, rule_id, Data quality flag]
+
+        # Iterate over columns and reorder them
+        column_order = ['Country', 'ISO Code', 'Region', 'Variable', 'Unit', 'Vehicle Type', 
+                    'Technology', 'Fuel', 'ID', 'Mode', 'Source', 'Service'] + column_list_names[2:] + ['Data quality flag']
         for index, row in df.iterrows():
             country_new = self.country_region_mapping(row['Economy Code'], regions)
             num_of_country =  len(country_new[row['Economy Code']])
@@ -277,6 +281,11 @@ class AtoWorkbook:
                         unit_value = pd.to_numeric(df.loc[index, column_list_names[idx]], errors='coerce')
                         final_unit = unit_value / remaining_attributes[3]
                         df_out_put.loc[index, column_list_names[idx]] = final_unit
+            # Add the "Data quality flag" column with a desired value (e.g., "!" or "!!")
+            df_out_put.loc[index, ['Data quality flag']] = '!'
+
+        # Reorder the columns
+        df_out_put = df_out_put[column_order]
                 
         return df_out_put
         
